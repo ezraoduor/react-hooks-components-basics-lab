08852710816f202cc2561937cf5ed1a7247ed22a
Ezra Oduor <ezrachurchill20@gmail.com>
2025-04-10 20:02:28 +0300

    intiail commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#home" id="home">I'm a link!</a>
     </nav>
   );
 }
@@ -16,15 +16,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
+
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
